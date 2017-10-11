94b9a38613ff0785372f343b257bd6e8abe28ec9
Germán Segura <german.andres.segura@gmail.com>
2017-10-11 14:29:07 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 
+export enum Condition { NEW, USED, DISCONTINUED}
+export enum Category { LAPTOP, PC}
+
+export class Product{
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  condition: Condition;
+  category: Category;
+}
+
 @Component({
   selector: 'app-root',
-  template: `
-    <p>
-      app Works!
-    </p>
+  template: `<h1>{{title}}</h1>
+  <h2>{{product.name}}</h2>
+    <label>Name</label>
+    <input [(ngModel)]="product.name" placeholder="name">
+    <div><label>Id:</label>{{product.id}}</div>
+    <div><label>Description::</label>{{product.description}}</div>
+    <div><label>Price:</label>{{product.price}}</div>
+    <div><label>Condition:</label>{{product.condition}}</div>
+    <div><label>Category:</label>{{product.category}}</div>
+    
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Products';
+  product: Product = {
+    id: 1,
+    name: 'Acer Laptop',
+    description: 'Core i7 8 GB RAM Solid State HD 250',
+    price: 600,
+    condition: Condition.NEW,
+    category: Category.LAPTOP
+  }
 }
